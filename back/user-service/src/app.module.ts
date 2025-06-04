cba6f828f0b140799ed7248755d3d346bd6d5647
nico servary <nicolas.servary123@gmail.com>
2025-06-04 12:35:19 +0200

    [Feat] adding docker compose with the database

--- back/user-service/src/app.module.ts
+++ back/user-service/src/app.module.ts
@@ -3,18 +3,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { User } from './user/user.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { join } from 'path';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'your_password',
-      database: 'auth_db',
-      entities: [User],
-      synchronize: true,
+    ConfigModule.forRoot({ 
+      isGlobal: true, 
+      envFilePath: join('../../.env'), 
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST', 'localhost'),
+        port: parseInt(configService.get<string>('DB_PORT') ?? '5432', 10),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        entities: [User],
+        synchronize: true,
+      }),
     }),
     AuthModule,
     UserModule,
